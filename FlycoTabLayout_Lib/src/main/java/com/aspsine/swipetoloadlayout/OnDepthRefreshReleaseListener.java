ff16ce4999c0e69e36e4693046fc6ee40d2ee92d
qiuxiaolong <472030374@qq.com>
2018-12-10 11:31:30 +0800

    添加Swipeloadlayout,修改添加下拉跳转二楼的能力

---
+++ FlycoTabLayout_Lib/src/main/java/com/aspsine/swipetoloadlayout/OnDepthRefreshReleaseListener.java
@@ -0,0 +1,12 @@
+package com.aspsine.swipetoloadlayout;
+
+import android.view.View;
+
+/**
+ * Created by qiuxiaolong on 2018/3/16.
+ */
+
+public interface OnDepthRefreshReleaseListener {
+    void depthRefreshRelease(View view);
+    void startDepthRefresh();
+}
---
+++ FlycoTabLayout_Lib/src/main/java/com/aspsine/swipetoloadlayout/SwipeRefreshHeaderLayout.java
@@ -0,0 +1,59 @@
+package com.aspsine.swipetoloadlayout;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.FrameLayout;
+
+/**
+ * Created by Aspsine on 2015/8/13.
+ */
+public class SwipeRefreshHeaderLayout extends FrameLayout implements SwipeRefreshTrigger, SwipeTrigger {
+
+    protected boolean depthRefreshEnable;
+
+    public boolean isDepthRefreshEnable() {
+        return depthRefreshEnable;
+    }
+
+    public void setDepthRefreshEnable(boolean depthRefreshEnable) {
+        this.depthRefreshEnable = depthRefreshEnable;
+    }
+
+    public SwipeRefreshHeaderLayout(Context context) {
+        this(context, null);
+    }
+
+    public SwipeRefreshHeaderLayout(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public SwipeRefreshHeaderLayout(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    public void onRefresh() {
+    }
+
+    @Override
+    public void onPrepare() {
+    }
+
+    @Override
+    public void onMove(int y, boolean isComplete, boolean automatic) {
+    }
+
+    @Override
+    public void onRelease() {
+
+    }
+
+    @Override
+    public void onComplete() {
+
+    }
+
+    @Override
+    public void onReset() {
+    }
+}
